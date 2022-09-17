b0a658d1369577b304bb6037417bf5a5d29f1ad5
MatsumaruTsusyoshi <73928886+MatsumaruTsuyoshi@users.noreply.github.com>
2022-09-17 21:19:59 +0900

    Revert "状態管理のパッケージ導入"

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,10 @@
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
-import 'package:hooks_riverpod/hooks_riverpod.dart';
-import 'package:spajam2022/ui/pages/sample_page.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp();
-  runApp(const ProviderScope(child: MyApp()));
+  runApp(const MyApp());
 }
 
 class MyApp extends StatelessWidget {
@@ -59,10 +57,6 @@ class _MyHomePageState extends State<MyHomePage> {
               '$_counter',
               style: Theme.of(context).textTheme.headline4,
             ),
-            ElevatedButton(
-                onPressed: () => Navigator.of(context).push(MaterialPageRoute(
-                    builder: (context) => const SamplePage())),
-                child: const Text('サンプルページへ移動')),
           ],
         ),
       ),
