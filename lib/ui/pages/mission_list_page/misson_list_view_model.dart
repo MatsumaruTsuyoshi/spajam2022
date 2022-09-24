1e6522d21110c9dfeff48f0e519d4cdd5a94eacb
MatsumaruTsuyoshi <strivesdfpx379@gmail.com>
2022-09-25 01:44:46 +0900

    feat: get firestore

--- lib/ui/pages/mission_list_page/misson_list_view_model.dart
+++ lib/ui/pages/mission_list_page/misson_list_view_model.dart
@@ -1,3 +1,4 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:freezed_annotation/freezed_annotation.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
 import 'package:spajam2022/model/mission.dart';
@@ -20,31 +21,39 @@ class MissionListPageViewModel extends StateNotifier<MissionListPageState> {
   }
 
   void setUp() {
-    state = state.copyWith(missions: [
-      Mission(
-        missionId: 1,
-        missionTitle: "八つ橋を食べよ",
-        missionIsClear: false,
-        missionIsDone: false,
-        missionDetail: "detail",
-        missionImagePath:"https://www.8284.co.jp/imgex.cgi?p=3&f=2.jpeg",
-      ),
-      Mission(
-          missionId: 2,
-          missionTitle: "鴨肉を食べよ",
-          missionIsClear: true,
-          missionIsDone: true,
-          missionDetail: "complete detail",
-          missionImagePath:
-              "https://cdn.pixabay.com/photo/2015/12/12/19/46/smoked-duck-1090027_1280.jpg"),
-      Mission(
-          missionId: 3,
-          missionTitle: "寺来た",
-          missionIsClear: false,
-          missionIsDone: true,
-          missionDetail: "complete review wait",
-          missionImagePath:
-              "https://cdn.pixabay.com/photo/2016/06/18/03/59/ginkaku-ji-temple-1464542_1280.jpg"),
-    ]);
+    final Stream<QuerySnapshot> missionsStream =
+        FirebaseFirestore.instance.collection('missions').snapshots();
+    missionsStream.listen((event) {
+      state = state.copyWith(
+          missions: event.docs
+              .map((e) => Mission.fromJson(e.data() as Map<String, dynamic>))
+              .toList());
+    });
+    // state = state.copyWith(missions: [
+    //   Mission(
+    //     missionId: 1,
+    //     missionTitle: "八つ橋を食べよ",
+    //     missionIsClear: false,
+    //     missionIsDone: false,
+    //     missionDetail: "detail",
+    //     missionImagePath: "https://www.8284.co.jp/imgex.cgi?p=3&f=2.jpeg",
+    //   ),
+    //   Mission(
+    //       missionId: 2,
+    //       missionTitle: "鴨肉を食べよ",
+    //       missionIsClear: true,
+    //       missionIsDone: true,
+    //       missionDetail: "complete detail",
+    //       missionImagePath:
+    //           "https://cdn.pixabay.com/photo/2015/12/12/19/46/smoked-duck-1090027_1280.jpg"),
+    //   Mission(
+    //       missionId: 3,
+    //       missionTitle: "寺来た",
+    //       missionIsClear: false,
+    //       missionIsDone: true,
+    //       missionDetail: "complete review wait",
+    //       missionImagePath:
+    //           "https://cdn.pixabay.com/photo/2016/06/18/03/59/ginkaku-ji-temple-1464542_1280.jpg"),
+    // ]);
   }
 }
